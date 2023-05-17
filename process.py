00aca3966d3302d84904fc45755849ad21828c7f
Diana <dianaaranda14@gmail.com>
2023-05-16 18:38:16 -0700

    added comments to explain code

--- process.py
+++ process.py
@@ -1,12 +1,13 @@
 log_file = open("um-server-01.txt")
 
 
-def generate_sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def generate_sales_reports(log_file):  #set function up 
+    for line in log_file:              #iterrates through every line in the file
+        line = line.rstrip()           #removes characted at  the right of a str 
+        day = line[0:3]                #assigning variable day to the first 3 items in the line
+        if day == "Mon":               #if day is Tue   
+            print(line)                #then print the line
 
 
-generate_sales_reports(log_file)
+generate_sales_reports(log_file)       #calling the function with log file as the parameters?
+
